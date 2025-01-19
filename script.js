583fa79060226396cba191b9dd05326900845fb4
Muhammad Hammad <hammad112116@gmail.com>
2025-01-19 15:10:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let doc=document.body.firstElementChild;
+console.log(doc);
+
+let docu=document.body.firstElementChild.childNodes;
+console.log(docu);
+
+const docume=document.body.firstElementChild.childNodes[0];
+console.log(docume);
+
+let docum=document.body.firstElementChild.child;
+console.log(docum);
+
+let hamma = document.querySelectorAll('.box');
+hamma.forEach(box => box.innerText = 'hello');
+console.log(hamma);
+
